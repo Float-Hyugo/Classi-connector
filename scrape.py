f3940e2feb780a03c4fa1ee4f3b30483df059aab
Float-Hyugo <76830994+Float-Hyugo@users.noreply.github.com>
2021-03-04 11:39:22 +0900

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -16,3 +16,12 @@ class Scraping():
   
   def login(self):
     self.driver = webdriver.Chrome('path to driverexe',options=options)
+    self.driver.get('https://auth.classi.jp/students')
+    id = self.driver.find_element_by_name('classi_id')
+    password = self.driver.find_element_by_name('password')
+    
+    id.send_keys(os.environ["USER_ID"])
+    password.send_keys(os.environ["PASS"])
+    
+    login_btn = self.driver.find_element_by_name('button')
+    
