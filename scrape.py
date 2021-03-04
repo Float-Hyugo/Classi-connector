e36449291a94bb314e132ad0b0a6c308b9d37acc
Float-Hyugo <76830994+Float-Hyugo@users.noreply.github.com>
2021-03-04 09:51:57 +0900

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,1 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions()
+import time
+import os
 
+os.environ["USER_ID"] = ""
+os.environ["PASS"] = ""
+
+#バックグラウンドでブラウザを起動するオプション
+option = ChormeOptions()
+options.add_argument('--headless')
+
+class Scraping():
+  def __init__(self):
+    self.driver = None
+  
+  def login(self):
+    self.driver = webdriver.Chrome('path to driverexe',options=options)
